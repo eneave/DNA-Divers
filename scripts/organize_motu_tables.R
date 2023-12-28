8897dec0fbe70e1e06195b43ee5b1c56cfca3484
eneave <e.f.neave@ljmu.ac.uk>
2023-12-28 17:15:39 +0000

    decontam shark tank samples

--- scripts/organize_motu_tables.R
+++ scripts/organize_motu_tables.R
@@ -26,7 +26,7 @@ library(decontam)
 library(phyloseq)
 library(ggplot2)
 #####
-## 1st Sequencing Run
+## 1st Sequencing Run ## return to UK MOTU tables once reference database has been updated
 #####
 # Convert to phyloseq objects 
 # partition MOTU table to only include counts
@@ -45,6 +45,66 @@ sample_info_tab <- merge(p1_names,p1_meta, by="seq_id", all.x = T) %>% ##merge b
 tax_tab <- p1 %>% select(c(id, kingdom, phylum, class, order, family, genus, species)) %>% 
                   replace_na(list(kingdom = "unassigned", phylum = "unassigned", class = "unassigned", order = "unassigned", family = "unassigned", genus = "unassigned", species = "unassigned")) %>%
                   column_to_rownames(var = "id")
+# create phyloseq object
+OTU = otu_table(as.matrix(count_tab), taxa_are_rows = TRUE)
+TAX = tax_table(as.matrix(tax_tab))
+SAM = sample_data(sample_info_tab)
+dataset <- merge_phyloseq(phyloseq(OTU, TAX), SAM)
+# code taken from tutorial: https://benjjneb.github.io/decontam/vignettes/decontam_intro.html#identify-contaminants---frequency
+# inspect library size
+df <- as.data.frame(sample_data(dataset)) # Put sample_data into a ggplot-friendly data.frame
+df$SampleReads <- sample_sums(dataset)
+df <- df[order(df$SampleReads),]
+df$Index <- seq(nrow(df))
+ggplot(data=df, aes(x=Index, y=SampleReads, color=sampletype2)) + geom_point() 
+# identify contaminants through prevalence 
+sample_data(dataset)$is.neg <- sample_data(dataset)$sampletype2 == "negative"
+# Default prevalence threshold of 0.1
+contamdf.prev <- isContaminant(dataset, method="prevalence", neg="is.neg")
+table(contamdf.prev$contaminant)
+head(which(contamdf.prev$contaminant))
+# Prevalence threshold of 0.5
+contamdf.prev05 <- isContaminant(dataset, method="prevalence", neg="is.neg", threshold=0.5)
+table(contamdf.prev05$contaminant)
+# extract rows which are possible contaminants and merge with taxonomy to inspect
+# 0.1
+contamdf.prevT <- contamdf.prev[contamdf.prev$contaminant == TRUE,]
+contamdf.prevT <- contamdf.prevT %>% rownames_to_column(var = "id")
+contamdf.prevT <- merge(p1, contamdf.prevT, by="id", all.x = F)
+# 0.5
+contamdf.prev05T <- contamdf.prev05[contamdf.prev05$contaminant == TRUE,]
+contamdf.prev05T <- contamdf.prev05T %>% rownames_to_column(var = "id")
+contamdf.prev05T <- merge(p1, contamdf.prev05T, by="id", all.x = F)
+## use 0.5 prevalence threshold
+p1_prev <- p1[!(p1$id %in% contamdf.prev05T$id),]
+write.csv(p1_prev, "C:/Users/beseneav/OneDrive - Liverpool John Moores University/PhD/DNAdivers/divers_methods_experiment/sintax_taxonomy/divmeth1_MOTU_decontam.csv")
+## remove generic named data, since same names will be used for different MOTU tables
+rm(count_tab,sample_info_tab,tax_tab,OTU,TAX,SAM,dataset,df)
+#####
+#####
+## Aquarium Samples - Shark tank
+#####
+# Convert to phyloseq objects 
+# partition MOTU table to only include counts
+# don't select extraction blank from June 19 because it's not usable
+View(p2e_aq)
+p2e_aq_v1 <- p2e_aq %>% select(c(id, sample.10Ae60BLUE_MPEtB:sample.11Ce_EBJun_12extblank,sample.11EeBLUE_FBEt_fieldb,sample.11Fe_negativePCRcontrol,sample.8CeBLUE_eDNA_FBblank:sample.9He60BLUE_MPEtA_)) %>%
+                        column_to_rownames(var = "id") # make MOTU ids row names
+count_tab <- p2e_aq_v1[rowSums(p2e_aq_v1[])>0,] # remove rows that sum to zero (they are there due to removal of non-aquarium samples)
+p2e_aq_v2 <- count_tab %>% rownames_to_column(var = "id") # have a version of count_tab with a MOTU ID column
+# list of seq_ids
+p2e_aq_names <- as.data.frame(colnames(p2e_aq_v2[-c(1)])) # 27 samples
+colnames(p2e_aq_names)[1] <- "seq_id" # can use list to compare to sample data to make sure they're in the same order
+###### THIS IS WHERE I STOPPED EDITING
+# read in metadata
+p1_meta <- read.csv("C:/Users/beseneav/OneDrive - Liverpool John Moores University/PhD/DNAdivers/divers_methods_experiment/p1_meta.csv")
+colnames(p1_meta)[1] <- "seq_id"
+sample_info_tab <- merge(p1_names,p1_meta, by="seq_id", all.x = T) %>% ##merge because 3 controls dropped out for having no reads assigned
+  column_to_rownames(var = "seq_id")
+# partition MOTU table to only include taxa
+tax_tab <- p1 %>% select(c(id, kingdom, phylum, class, order, family, genus, species)) %>% 
+  replace_na(list(kingdom = "unassigned", phylum = "unassigned", class = "unassigned", order = "unassigned", family = "unassigned", genus = "unassigned", species = "unassigned")) %>%
+  column_to_rownames(var = "id")
 # create phyloseq object
 OTU = otu_table(as.matrix(count_tab), taxa_are_rows = TRUE)
 TAX = tax_table(as.matrix(tax_tab))
@@ -79,6 +139,8 @@ contamdf.prev05T <- merge(p1, contamdf.prev05T, by="id", all.x = F)
 ## use 0.5 prevalence threshold
 p1_prev <- p1[!(p1$id %in% contamdf.prev05T$id),]
 write.csv(p1_prev, "C:/Users/beseneav/OneDrive - Liverpool John Moores University/PhD/DNAdivers/divers_methods_experiment/sintax_taxonomy/divmeth1_MOTU_decontam.csv")
-
+## remove generic named data, since same names will be used for different MOTU tables
+rm(count_tab,sample_info_tab,tax_tab,OTU,TAX,SAM,dataset,df)
+#####
 
 
